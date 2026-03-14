a176ecb25461b93c73e7979b6f62d340540018d5
Rendi Mulia Setiawan <rendirudi106@gmail.com>
2026-03-14 14:09:51 +0800

    membuat halaman Edukasi

--- app/src/main/java/com/example/it_project2/EdukasiActivity.java
+++ app/src/main/java/com/example/it_project2/EdukasiActivity.java
@@ -1,13 +1,42 @@
 package com.example.it_project2;
 
-
 import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class EdukasiActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // Layout akan diisi nanti
+        setContentView(R.layout.activity_edukasi);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottomNav);
+        bottomNav.setSelectedItemId(R.id.nav_edukasi);
+
+        bottomNav.setOnItemSelectedListener(item -> {
+            int id = item.getItemId();
+            if (id == R.id.nav_home) {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                overridePendingTransition(0, 0);
+                return true;
+            } else if (id == R.id.nav_riwayat) {
+                startActivity(new Intent(getApplicationContext(), RiwayatActivity.class));
+                overridePendingTransition(0, 0);
+                return true;
+            } else if (id == R.id.nav_kontrol) {
+                // startActivity(new Intent(getApplicationContext(), KontrolActivity.class));
+                // overridePendingTransition(0, 0);
+                return true;
+            } else if (id == R.id.nav_edukasi) {
+                return true;
+            } else if (id == R.id.nav_profile) {
+                startActivity(new Intent(getApplicationContext(), ProfileActivity.class));
+                overridePendingTransition(0, 0);
+                return true;
+            }
+            return false;
+        });
     }
 }
