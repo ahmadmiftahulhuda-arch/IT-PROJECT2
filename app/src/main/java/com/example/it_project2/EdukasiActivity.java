1dde391952b5d9522ca2db7eb6edb6f2b6a4fe17
aprwrdhnii <apriw2005@gmail.com>
2026-03-28 03:00:26 +0800

    integrasi firebase buat grafik riwayat suhu, fallback data dummy kalo firebase kosong

--- app/src/main/java/com/example/it_project2/EdukasiActivity.java
+++ app/src/main/java/com/example/it_project2/EdukasiActivity.java
@@ -3,6 +3,8 @@ package com.example.it_project2;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
+
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class EdukasiActivity extends AppCompatActivity {
@@ -12,6 +14,11 @@ public class EdukasiActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edukasi);
 
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+
+        // Bottom Navigation
         BottomNavigationView bottomNav = findViewById(R.id.bottomNav);
         bottomNav.setSelectedItemId(R.id.nav_edukasi);
 
@@ -20,20 +27,24 @@ public class EdukasiActivity extends AppCompatActivity {
             if (id == R.id.nav_home) {
                 startActivity(new Intent(getApplicationContext(), MainActivity.class));
                 overridePendingTransition(0, 0);
+                finish();
                 return true;
             } else if (id == R.id.nav_riwayat) {
                 startActivity(new Intent(getApplicationContext(), RiwayatActivity.class));
                 overridePendingTransition(0, 0);
+                finish();
                 return true;
             } else if (id == R.id.nav_kontrol) {
                 startActivity(new Intent(getApplicationContext(), KontrolActivity.class));
                 overridePendingTransition(0, 0);
+                finish();
                 return true;
             } else if (id == R.id.nav_edukasi) {
                 return true;
             } else if (id == R.id.nav_profile) {
                 startActivity(new Intent(getApplicationContext(), ProfileActivity.class));
                 overridePendingTransition(0, 0);
+                finish();
                 return true;
             }
             return false;
